ba9b7ddc5156acc18c567e30f9469006c97cf010
SamuelAllen76 <sra76@cornell.edu>
2025-10-24 00:29:25 +0000

    my first commit

--- shoe_data.R
+++ shoe_data.R
@@ -1,5 +1,5 @@
 # Get Experimental Data
-df_shoes = read.csv("Shoe_Data - Experimental (1).csv")
+df_shoes = read.csv("Shoe_Data - Experiment.csv")
 
 # Get Source Data
 df_ref = read.csv("Shoe_Data - RunRepeat (1).csv")
@@ -20,6 +20,9 @@ df_SA <- data.frame(specimen = vec_names,shore_hardness = vec)
 
 # Create standard deviation chart
 ggs(df_SA$specimen,df_SA$shore_hardness,xlab="Specimen")
+
+# Percent Change of SA
+RSA = (df_ref$New_SA-df_shoes$Avg_Retired_SA)/df_ref$New_SA*100
 
 # Save to Github
 credentials::set_github_pat()
